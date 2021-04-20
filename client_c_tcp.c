bdfeebe8ab4f116c25ec7128bee9bf0ba4af23ef
veltsonbastien <bastienveltson@outlook.com>
2021-04-19 20:08:57 -0700

    formatted and added comments

--- client_c_tcp.c
+++ client_c_tcp.c
@@ -1,3 +1,4 @@
+// Code from TCP Example Code given on Gauchospace
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,76 +6,59 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 #include <time.h>
 
-void error(const char *msg)
-{
-    perror(msg);
+void error(const char * msg) {
+  perror(msg);
+  exit(0);
+}
+
+int main(int argc, char * argv[]) {
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent * server;
+
+  char buffer[256];
+  if (argc < 3) {
+    fprintf(stderr, "usage %s hostname port\n", argv[0]);
     exit(0);
+  }
+  portno = atoi(argv[2]);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    error("ERROR opening socket");
+  server = gethostbyname(argv[1]);
+  if (server == NULL) {
+    fprintf(stderr, "ERROR, no such host\n");
+    exit(0);
+  }
+  bzero((char * ) & serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char * ) server -> h_addr,
+    (char * ) & serv_addr.sin_addr.s_addr,
+    server -> h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd, (struct sockaddr * ) & serv_addr, sizeof(serv_addr)) < 0)
+    error("ERROR connecting");
+  printf("Enter string: ");
+  bzero(buffer, 256);
+  fgets(buffer, 255, stdin);
+  n = write(sockfd, buffer, strlen(buffer));
+  if (n < 0)
+    error("ERROR writing to socket");
+  bzero(buffer, 256);
+  n = read(sockfd, buffer, 255);
+  if (n < 0) error("ERROR reading from socket");
+  while (strlen(buffer) > 0) { 
+    //Continue to print messages from server while we are still reading from it:
+    printf("%s", "From server: ");
+    printf("%s\n", buffer);
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+  }
+  //Close the socket:
+  close(sockfd);
+  return 0;
 }
-
-//Delay code taken from: https://www.geeksforgeeks.org/time-delay-c/
-void delay(int number_of_seconds)
-{
-    // Converting time into milli_seconds
-    int milli_seconds = number_of_seconds;
-
-    // Storing start time
-    clock_t start_time = clock();
-
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds)
-        ;
-}
-
-
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Enter string: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    //while(strlen(buffer)>1){
-    while(strlen(buffer)>0){
-     printf("%s", "From server: ");
-     printf("%s\n",buffer);
-     bzero(buffer,256);
-     n=read(sockfd, buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-    }
-    if (n < 0) 
-         error("ERROR reading from socket");
-    close(sockfd);
-    return 0;
-    }
